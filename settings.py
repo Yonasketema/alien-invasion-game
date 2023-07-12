12adf7c1c09706663f7a1d2e88d3e936d74ac9a1
yonasketema <yonask.kitaw@gmail.com>
2023-07-12 11:52:30 +0300

    add ship image

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 class Settings:
     def __init__(self):
-        self.screen_width = 1000
-        self.screen_height = 800
+        self.screen_width = 900
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
